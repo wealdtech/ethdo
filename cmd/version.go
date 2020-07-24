1f0aeac9b41d3b0b873dcf73d57d3b07942e1a46
Jim McDonald <Jim@mcdee.net>
2020-07-24 13:34:33 +0100

    Bump version

--- cmd/version.go
+++ cmd/version.go
@@ -22,7 +22,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-var ReleaseVersion = "local build from v1.5.0"
+var ReleaseVersion = "local build from v1.5.1"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
